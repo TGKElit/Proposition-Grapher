e5da0bbc3719e4a89430c959d479d434749bb464
TGKElit <tgkelit@gmail.com>
2023-10-19 20:12:31 +0200

    regristration implemented

--- back_end/src/main.rs
+++ back_end/src/main.rs
@@ -1,32 +1,68 @@
 use poem::{listener::TcpListener, endpoint::StaticFilesEndpoint, Route};
-use poem_openapi::{param::Query, payload::Json, OpenApi, OpenApiService};
+use poem_openapi::{payload::Json, OpenApi, OpenApiService, Object};
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
+use bcrypt::{hash, verify};
 
 struct Api;
 
+#[derive(Object)]
+struct RegisterRequest {
+    email: String,
+    username: String,
+    password: String
+}
+struct User {
+    email: String,
+    username: String,
+    password_hash: String
+}
+
 #[OpenApi]
 impl Api {
-    #[oai(path = "/hello", method = "get")]
-    async fn index(&self, name: Query<Option<String>>) -> Json<String> {
-        match name.0 {
-            Some(name) => Json(format!("hello, {}!", name)),
-            None => Json("hello!".to_string()),
-        }
+    #[oai(path = "/register", method = "post")]
+    async fn register(&self, request: Json<RegisterRequest>) -> Json<String> {
+        let pool = database_connection().await;
+
+        let password_hash = hash(request.0.password, 15).expect("Hashing failed");
+
+        sqlx::query_as!(
+            User,
+            "insert into users(email, username, password_hash) values ($1, $2, $3)",
+            request.0.email, request.0.username, password_hash
+            )
+            .fetch_all(&pool)
+            .await.expect("Unable to insert a user");
+
+            Json(format!("hello, {}!", request.0.username))
     }
+}
+
+async fn database_connection() -> Pool<Postgres> {
+    let db_url = std::env::var("DATABASE_URL").expect("Unable to read DATABASE_URL env var");
+  
+    PgPoolOptions::new()
+        .max_connections(100)
+        .connect(&db_url)
+        .await.expect("Unable to connect to Postgres")
+
 }
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+
+    dotenv::dotenv().expect("Unable to load environment variables from .env file");
+    
     let api_service =
         OpenApiService::new(Api, "Hello World", "1.0").server("/api");
-    let app = Route::new().nest(
-        "/",
-        StaticFilesEndpoint::new("../front_end/dist/")
-            .show_files_listing()
-            .index_file("index.html"),
-    ).nest("/api", api_service);
-    
+    let front_end = StaticFilesEndpoint::new("../front_end/dist/")
+        .index_file("index.html");
+    let app = Route::new()
+        .nest("/", front_end)
+        .nest("/api", api_service);
+
+
     poem::Server::new(TcpListener::bind("127.0.0.1:3000"))
         .run(app)
         .await
-
 }
+
--- front_end/src/App.svelte
+++ front_end/src/App.svelte
@@ -1,36 +1,58 @@
 <script lang="ts">
-  import Counter from './lib/Counter.svelte'
 
- let response = "";
- let inputValue: String;
-  const callApi = (name: String) => {
-    let query = "";
-    if (name) {
-      query = "?name=" + name;
-    } 
-    fetch("/api/hello" + query)
-      .then(response => response.json())
-      .then(data => {
-        response = data;
-      }).catch(error => {
+let response = "";
+let email: string;
+let username: string;
+let password: string;
+
+const callApi = (email: string, username: string, password: string) => {
+    fetch("/api/register", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "email": email,
+        "username": username,
+        "password": password
+      })
+    })
+    .then(response => response.json())
+    .then(data => {
+      response = data;
+      console.log(response);
+    }).catch(error => {
         console.log(error);
         return [];
-      });
-    
-  }
+    });
+  
+}
 </script>
 
 <main>
-  
   <h1>Proposition Grapher</h1>
-
-  <input type="text" bind:value={inputValue}>
-  <button on:click={() => {callApi(inputValue)}}></button>
+  <section>
+    <label for="email">Email:</label>
+    <input type="text" name="email" bind:value={email}>
+    <label for="username">Username:</label>
+    <input type="text" name="username" bind:value={username}>
+    <label for="password">Password:</label>
+    <input type="password" name="password" bind:value={password}>
+    <button on:click={() => {callApi(email, username, password)}}>Register</button>
+  </section>
 
   <p>{response}</p>
 
 </main>
-
-<style>
   
+<style>
+  section {
+    height: 40vh;
+    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-between;
+    background-color: #EEE;
+  }
 </style>
